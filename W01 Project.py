2717746775ee65b083b5664becc465e69cf252d7
JRoy99 <113636787+JRoy99@users.noreply.github.com>
2022-09-15 15:09:53 -0600

    First version of Tic-Tac-Toe game, single play on a 3x3 board with 2 players. Win and draw conditions checked

--- W01 Project.py
+++ W01 Project.py
@@ -1,21 +1,91 @@
 # Name: James Roy
+# Date: 9/15/2022
 # Assignment: W01 Prove; Tic-Tac-Toe
+# Description: Allows two players to play tic-tac-toe on a 3x3 board
+
+import math
+
+def drawBoard(board_list):
+    for i in range(9):
+        print(board_list[i], end="")
+        if (i+1) % 3 != 0:
+            print("|", end="")
+        elif (i+1) % 3 == 0 and i != 8:
+            print("\n-+-+-")
+    print("\n")
+    pass
+
+def playerTurn(board_list, player):
+    turn_over = False
+
+    while not(turn_over):
+        move = int(input(player + "\'s turn to choose a square (1-9): "))
+        try:
+            board_list[move-1] = player
+        except: 
+            print("Invalid Selection")
+        else:     
+            if not(isinstance(board_list[move-1], str)):
+                turn_over = True
+            else:
+                print("Invalid Selection")
+    pass
+
+def checkWin(board_list):
+
+    #Check horizontal win conditions
+    if board_list[0] == board_list[1] == board_list[2]:
+        return True
+    if board_list[3] == board_list[4] == board_list[5]:
+        return True
+    if board_list[6] == board_list[7] == board_list[8]:
+        return True
+
+    #Check vertical win conditions
+    if board_list[0] == board_list[3] == board_list[6]:
+        return True
+    if board_list[1] == board_list[4] == board_list[7]:
+        return True
+    if board_list[2] == board_list[5] == board_list[8]:
+        return True
+
+    #Check diagonal win conditions
+    if board_list[0] == board_list[4] == board_list[8]:
+        return True
+    if board_list[2] == board_list[4] == board_list[6]:
+        return True
+    
+    return False
 
 
-def drawBoard(board_array):
-    for i in range(0, 2):
-        print(board_array[i])
-        if i < 2:
-            print("|")
-
+def checkDraw(board_list):
+    for i in board_list:
+        if isinstance(board_list[move-1], int):
+            return False
+    return True
 
 def main():
     game_over = False
-    board_array = [1, 2, 3,
+    player_1 = "x"
+    player_2 = "o"
+    board_list = [1, 2, 3,
                     4, 5, 6,
                     7, 8, 9]
-    print(board_array)
-    drawBoard(board_array)
+
+    #while not(game_over):
+    drawBoard(board_list)
+    playerTurn(board_list, player_1)
+    if checkWin(board_list):
+        game_over = True
+        print(player_1 + " wins!")
+    elif checkDraw(board_list):
+        game_over = True
+        print("It's a draw!")
+    playerTurn(board_list, player_2)
+    if checkWin(board_list):
+        game_over = True
+        print(player_2 + " wins!")
+
 
 if __name__ == "__main__":
     main()
